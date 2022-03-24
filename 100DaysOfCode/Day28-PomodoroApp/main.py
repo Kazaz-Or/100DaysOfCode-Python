aa9b99b0f4fc47b1c93e3bf510cd73f74c3ed403
Kazaz-Or <83350680+Kazaz-Or@users.noreply.github.com>
2022-03-24 18:05:38 +0200

    Day 28 - Pomodoro Application

---
+++ 100DaysOfCode/Day28-PomodoroApp/main.py
@@ -0,0 +1,87 @@
+import tkinter
+import math
+
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 0
+timer = None
+
+
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer", fg=GREEN, font=(FONT_NAME, 50))
+    check_marks.config(text="")
+    global reps
+    reps = 0
+
+
+def start_timer():
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        countdown(long_break_sec)
+        title_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        countdown(short_break_sec)
+        title_label.config(text="Break", fg=PINK)
+    else:
+        countdown(work_sec)
+        title_label.config(text="Work", fg=GREEN)
+
+
+def countdown(count):
+    count_minute = math.floor(count / 60)
+    count_seconds = count % 60
+
+    if count_seconds < 10:
+        count_seconds = f"0{count_seconds}"
+
+    canvas.itemconfig(timer_text, text=f"{count_minute}:{count_seconds}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, countdown, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        for i in range(math.floor(reps/2)):
+            marks += "✓"
+        check_marks.config(text=marks)
+
+
+window = tkinter.Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50)
+
+
+title_label = tkinter.Label(text="Timer", fg=GREEN, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
+
+canvas = tkinter.Canvas(width=200, height=224)
+tomato_img = tkinter.PhotoImage(file="tomato.png")
+canvas.create_image(103, 112, image=tomato_img)
+timer_text = canvas.create_text(103, 130, text="00:00", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+start_button = tkinter.Button(text="Start", command=start_timer)
+start_button.grid(column=0, row=2)
+
+reset_button = tkinter.Button(text="Reset", command=reset_timer)
+reset_button.grid(column=2, row=2)
+
+check_marks = tkinter.Label(fg=GREEN)
+check_marks.grid(column=1, row=3)
+
+
+window.mainloop()

d381ff49c90cfc39a07db8a12719962370531983
Or Kazaz <83350680+Kazaz-Or@users.noreply.github.com>
2022-03-29 13:10:40 +0300

    Day33 - ISS Overhead Notifier (#20)

--- 100DaysOfCode/Day33-IssOverheadNotifier/main.py
+++ 100DaysOfCode/Day33-IssOverheadNotifier/main.py
@@ -1,28 +1,48 @@
-from tkinter import *
 import requests
+from datetime import datetime
+import smtplib
+import time
+
+MY_LAT = 32.070808
+MY_LONG = 34.812881
+EMAIL = "orkazazgithub@gmail.com"
+PASS = "notarealpass"
 
 
-def get_quote():
-    response = requests.get("https://api.kanye.rest")
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
     response.raise_for_status()
     data = response.json()
-    quote = data["quote"]
-    canvas.itemconfig(quote_text, text=quote)
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
+        return True
 
 
-window = Tk()
-window.title("Kanye Says...")
-window.config(padx=50, pady=50)
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-canvas = Canvas(width=300, height=414)
-background_img = PhotoImage(file="background.png")
-canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
-canvas.grid(row=0, column=0)
+    time_now = datetime.now().hour
 
-kanye_img = PhotoImage(file="kanye.png")
-kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
-kanye_button.grid(row=1, column=0)
+    if time_now >= sunset or time_now <= sunrise:
+        return True
 
 
-window.mainloop()
+while True:
+    time.sleep(60)
+    if is_iss_overhead() and is_night():
+        connection = smtplib.SMTP("smtp.gmail.com")
+        connection.starttls()
+        connection.login(EMAIL, PASS)
+        connection.sendmail(from_addr=EMAIL, to_addrs=EMAIL, msg="Subject:Look Up\n\nThe ISS is above you in the sky")

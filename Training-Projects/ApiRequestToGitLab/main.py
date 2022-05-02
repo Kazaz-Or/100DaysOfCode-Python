f78218d9432255707ed97fb41800e1c1ec420854
Or Kazaz <83350680+Kazaz-Or@users.noreply.github.com>
2022-05-02 13:13:57 +0300

    Delete Training-Projects/ApiRequestToGitLab directory

--- Training-Projects/ApiRequestToGitLab/main.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-response = requests.get("https://gitlab.com/api/v4/users/nanuchi/projects")
-my_projects = response.json()
-
-for project in my_projects:
-    print(f"Project Name: {project['name']}\n Project URL: {project['web_url']}\n")
